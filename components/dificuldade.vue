1a1e04756f65a4a30ede6e6bb5fdbf414d34dd48
Gabriel Moura <email.gabriel.moura@gmail.com>
2025-02-04 16:26:23 -0300

    fix: next card

---
+++ components/dificuldade.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-bottom-sheet v-model="sheet">
+    <v-card class="rounded-t-xl">
+      <div class="d-flex justify-space-between pa-2">
+        <b class="w-100 text-center">Dificuldade do card </b>
+      </div>
+      <VCardText class="d-flex ga-2 height">
+        <VCard rounded="lg" class="m-4 d-novo d-flex flex-column justify-center">
+          <p>De novo</p>
+          <span>agora</span>
+        </VCard>
+        <VCard rounded="lg" class="m-4 d-dificil d-flex flex-column justify-center">
+          <p>Dificil</p>
+          <span> 8m</span>
+        </VCard>
+        <VCard rounded="lg" class="m-4 d-bom d-flex flex-column justify-center">
+          <p>Bom</p>
+          <span> 15m</span>
+        </VCard>
+        <VCard rounded="lg" class="m-4 d-facil d-flex flex-column justify-center">
+          <p>Facil</p>
+          <span> 4d</span>
+        </VCard>
+      </VCardText>
+    </v-card>
+  </v-bottom-sheet>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      sheet: true,
+    };
+  },
+};
+</script>
+<style scoped>
+.card {
+  background: #282725;
+}
+.d-novo {
+  background: #351f20;
+  width: 25%;
+  height: 50px;
+}
+.d-dificil {
+  background: #3f3720;
+  width: 25%;
+  height: 50px;
+}
+.d-bom {
+  background: #2c361b;
+  width: 25%;
+  height: 50px;
+}
+.d-facil {
+  background: #232833;
+  width: 25%;
+  height: 50px;
+}
+p {
+  font-size: 12px;
+  text-align: center;
+}
+span {
+  font-size: 10px;
+  text-align: center;
+  width: 100%;
+  line-height: 1;
+}
+.height {
+  height: 130px;
+}
+</style>
---
+++ components/flipCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="flip-card" @click="rotate = !rotate">
+    <div class="flip-card-inner" :class="{ rotated: rotate }">
+      <VCard rounded="xl" class="flip-card-front">front</VCard>
+      <VCard rounded="xl" class="flip-card-back">back</VCard>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rotate: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.flip-card {
+  background-color: transparent;
+  width: 100%;
+  height: 80%;
+  inset: 0;
+  margin: auto;
+  perspective: 1000px;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.6s;
+  transform-style: preserve-3d;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+.rotated {
+  transform: rotateY(180deg);
+}
+
+.flip-card-front,
+.flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  margin-top: 50px;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+
+.flip-card-front {
+  background: #282725;
+  color: rgb(255, 255, 255);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.flip-card-back {
+  background-color: #626262;
+  color: white;
+  transform: rotateY(180deg);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
