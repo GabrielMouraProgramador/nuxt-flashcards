9abde2c34078cffecc4e7e51a75f0d9da01249f5
Gabriel Moura <email.gabriel.moura@gmail.com>
2025-02-06 10:07:18 -0300

    fix: grid

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,7 @@
 <template>
   <v-app>
     <AppBar />
-    <VMain>
-      <slot />
-    </VMain>
+    <slot />
     <NavBar />
   </v-app>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,17 @@
 <template>
   <VCard rounded="xl" class="ma-4 card-height">
-    <div>
+    <div class="overflow-y-auto">
       <NewDeck @refresh="$refs.listDeck.allDecks()" ref="newDeck" />
       <ListDeck ref="listDeck" />
     </div>
-    <VCardActions class="justify-end">
-      <VBtn
-        color="primary"
-        icon="mdi-plus"
-        variant="flat"
-        class="ma-2"
-        @click="$refs.newDeck.sheet = true"
-      ></VBtn>
-    </VCardActions>
+
+    <VBtn
+      color="primary"
+      icon="mdi-plus"
+      variant="flat"
+      class="ma-2 add-float"
+      @click="$refs.newDeck.sheet = true"
+    ></VBtn>
   </VCard>
 </template>
 
@@ -30,18 +29,21 @@ export default {
       },
     };
   },
-  methods: {
-    teste() {
-      alert("aqui");
-    },
-  },
 };
 </script>
 <style scoped>
 .card-height {
-  height: calc(100% - 140px) !important;
+  height: 100%;
+  max-height: calc(100vh - 150px);
+  margin-top: 70px !important;
   display: flex;
   justify-content: space-between;
   flex-direction: column;
+  position: relative;
+}
+.add-float {
+  position: absolute;
+  bottom: 0px;
+  right: 25px;
 }
 </style>
