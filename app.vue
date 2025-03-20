072bb273186f2ad37c262a3b6e059bbc92638927
Gabriel Moura <email.gabriel.moura@gmail.com>
2025-03-20 09:41:57 -0300

    deat: add pull-to-refresh

--- app.vue
+++ app.vue
@@ -1,19 +1,30 @@
 <template>
   <NuxtLayout>
     <v-app>
-      <NuxtPage />
+      <v-pull-to-refresh :pull-down-threshold="pullDownThreshold" @load="load">
+        <NuxtPage />
+      </v-pull-to-refresh>
     </v-app>
   </NuxtLayout>
 </template>
 <script setup>
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router"; // Importa o useRouter
 import { useHead } from "#imports";
 
 const route = useRoute();
+const router = useRouter(); // Aqui, você acessa o router
 
 useHead({
   bodyAttrs: {
     id: () => route.name || route.path.replace(/\//g, "") || "home",
   },
 });
+
+async function load({ done }) {
+  await new Promise((resolve) => setTimeout(() => resolve(), 2000));
+
+  router.go(0); // Isso recarrega a página
+
+  done("ok");
+}
 </script>
